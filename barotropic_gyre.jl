73bd28b396d4454762c2a6aeadbcf1c4ccc1c18b
Gregory Wagner <wagner.greg@gmail.com>
2021-12-11 20:37:29 -0700

    Adds barotorpic gyre

---
+++ barotropic_gyre.jl
@@ -0,0 +1,27 @@
+using Oceananigans
+
+# Grid
+Nx = Ny = 60
+Lx = Ly = 1200kilometers
+
+grid = RectilinearGrid(size = (Nx, Ny),
+                       x = (-Lx/2, Ly/2),
+                       y = (-Ly/2, Ly/2),
+                       topology = (Bounded, Bounded, Flat))
+
+# Coriolis and viscosity
+coriolis = BetaPlane(f₀=1e-4, β=1e-11)
+closure = IsotropicViscosity(ν=4e2)
+
+# Top boundary condition
+wind_stress(x, y, t) = -1e-4 * cos(π * x / 600kilometers)
+u_bcs = FieldBoundaryConditions(top=FluxBoundaryConditions(wind_stress))
+
+# Model
+model = NonhydrostaticModel(; grid, coriolis, closure,
+                            boundary_conditions = (; u=u_bcs))
+                           
+simulation = Simulation(model, Δt = 3600, stop_time = 1years)
+
+run!(simulation)
+
